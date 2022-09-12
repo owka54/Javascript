c5f7c40ef899a6c16628ceeb56cc6cf98f968f5f
Owen K <owen.kane54@gmail.com>
2022-09-12 11:41:16 +0100

    Add random location generator function

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 // Generate random words
 const food = () => {
-    let foods = ['cereal', 'porridge', 'pancakes', 'full english', 'toast', 'soup', 'fruit']
+    let foods = ['cereal', 'porridge', 'pancakes', 'full english', 'toast', 'soup', 'fruit'];
 
-    return foods[Math.floor(Math.random() * foods.length)]
+    return foods[Math.floor(Math.random() * foods.length)];
 }
 
 // console.log(food());
+
+const location = () => {
+    let locations =['outside', 'living room', 'kitchen', 'bedroom', 'cafe', 'park'];
+
+    return locations[Math.floor(Math.random() * locations.length)];
+}
+
+console.log(location());
